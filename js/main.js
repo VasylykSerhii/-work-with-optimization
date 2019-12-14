1a37cea947cc91b39f169af1fc590b6044e03abb
Serhii <vasylyk29@gmail.com>
2019-12-14 15:23:12 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', function () {
+  'use strict';
+
+
+  $('.review-slider').slick({
+    dots: false,
+    nextArrow: '.arrow-right',
+    prevArrow: '.arrow-left',
+  });
+
+});
